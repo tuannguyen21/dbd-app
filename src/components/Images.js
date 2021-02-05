f3c304664036ccb83e551290e0b95a164799f789
tuannguyen21 <51152321+tuannguyen21@users.noreply.github.com>
2021-02-04 21:21:07 -0600

    Revert "Test commit"
    This reverts commit edb0c966ce9469e5ecef6a64e18bb303e7ae08d4.

--- src/components/Images.js
+++
@@ -1,7 +0,0 @@
-export var perks=[];
-
-for (let i=0; i<80; i++) {
-  var path = require("../../public/perks/perk" + i + ".png");
-  var perk = {pathVar: path, valueVar: i};
-  perks.push(perk);
-}
--- src/index.js
+++ src/index.js
@@ -1,22 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Roll from "./components/Roll";
-
-class Site extends React.Component {
-
-  render() {
-    return (
-      <div className="Site">
-        <div className="Roll">
-          <Roll />
-        </div>
-      </div>
-    );
-  }
-}
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <Site />,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
